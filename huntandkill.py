92e8238f9f3472173e53ef4d59dc98a8ddd4a106
sabah-z-ahmad <55769577+sabah-z-ahmad@users.noreply.github.com>
2022-02-28 23:36:30 -0500

    bug fix

--- huntandkill.py
+++ huntandkill.py
@@ -9,6 +9,10 @@ import pygame
 def walk(surface, g, current_cell, animate):
     # Loop until the current cell has no unvisited neighbors
     while len(current_cell.unvisited_neighbors) > 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                
         # Choose a random unvisited neighbor as a new cell
         new_cell = current_cell.unvisited_neighbors[int(random.random() * len(current_cell.unvisited_neighbors)) - 1]
 
